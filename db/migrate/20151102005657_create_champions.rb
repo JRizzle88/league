c3ea455256d6d5833b2c9e81d913d14bbdf405b2
Stephen Hughes <hughes0687@gmail.com>
2015-11-01 19:05:57 -0600

    set up champs

--- db/migrate/20151102005657_create_champions.rb
+++ db/migrate/20151102005657_create_champions.rb
@@ -1,7 +1,10 @@
 class CreateChampions < ActiveRecord::Migration
   def change
     create_table :champions do |t|
-
+      t.string :champion_id
+      t.boolean :active
+      t.boolean :free
+      t.boolean :ranked
       t.timestamps null: false
     end
   end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151102003313) do
+ActiveRecord::Schema.define(version: 20151102005657) do
 
   create_table "champions", force: :cascade do |t|
     t.string   "champion_id"
